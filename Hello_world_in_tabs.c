53e490260e4ae776cca4977d1ec91fb5ac46a846
Praneeth Sugandh <153162968+praneeth-2504@users.noreply.github.com>
2024-05-16 15:35:44 +0530

    Hello world with tab

---
+++ Hello_world_in_tabs.c
@@ -0,0 +1,11 @@
+// Write a program to print Hello World with tab
+
+#include <stdio.h>
+int main()
+{
+   // Try out your code here
+   printf("Hello, World!");
+   return 0;
+}
+
+// This C code is a simple program that prints "Hello, World!" to the console using the printf() function from the stdio.h library.
