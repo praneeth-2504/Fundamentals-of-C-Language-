e454943902b0274e4c2020b9008d3f98c2afd15c
Praneeth Sugandh <153162968+praneeth-2504@users.noreply.github.com>
2024-05-18 16:35:09 +0530

    Add files via upload

---
+++ Hello_World_with_a_new_line.c
@@ -0,0 +1,17 @@
+/*
+Hello World with a new line
+
+Write a program to print the given sample output.
+*/
+
+#include <stdio.h>
+int main()
+{
+   // Try out your code here
+   printf("Hello World\nHello World");
+   return 0;
+}
+
+/*
+This C program prints the string "Hello World" twice, each on a new line, using the `printf()` function. The `\n` character sequence represents a newline, causing the second "Hello World" to be printed on a separate line from the first. Finally, the program returns 0, indicating successful execution.
+*/
